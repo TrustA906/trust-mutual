68c99f6488d10983b0c2f7312dda6b9d17754aee
ronran <rongran@wushenggu.com>
2022-01-13 16:41:56 +0800

    增加uploadOss

---
+++ packages/trust-util/src/uploadOss/type.ts
@@ -0,0 +1,9 @@
+export interface uploadConfig {
+  access_key_id: string;
+  access_key_secret: string;
+  filename: string;
+  endpoint: string;
+  file: any;
+  url: string;
+  security_token?: string;
+}
---
+++ packages/trust-util/tests/uploadOss.test.js
@@ -0,0 +1,1 @@
+import { uploadOss } from '../src';
