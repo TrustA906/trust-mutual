7bcaff8390ede220f11a1227614a069da2cafb7f
rongran <rongran@wushenggu.com>
2022-12-30 14:46:54 +0800

    补充文档

--- packages/trust-util/src/verify/index.ts
+++ packages/trust-util/src/verify/index.ts
@@ -4,5 +4,5 @@ export { default as isEmail } from './isEmail';
 export { default as isForeigner } from './isForeigner';
 export { default as isPhone } from './isPhone';
 export * from './isName/index';
-export * from './isCardID/index';
+export * as isCardID from './isCardID/index';
 export * from './isAddress/index';
